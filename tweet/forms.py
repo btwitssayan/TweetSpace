c0619e52d919d848df1f85527620b5d2a498c21b
Sayan Golder <sayangolder2004@gmail.com>
2024-09-08 17:16:26 +0530

    Your commit message

---
+++ tweet/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ['text', 'image']
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
